2bcb88f2621f89835426bd95feaadaea0e656c41
PedroMoreira18 <psblm@cesar.school>
2025-06-10 13:10:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let titulo = document.getElementById("titulo");
+titulo.textContent = "Meus Trabalhos";
+titulo.style.color= "white";
+titulo.style.fontFamily = "sans-serif";
+titulo.style.fontSize = "50px";
